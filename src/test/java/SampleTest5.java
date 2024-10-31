93d40f53c7d12c3a046cddfebde84ccbdaa21c9a
Olaismail316 <olaismail613@gmail.com>
2024-10-31 13:35:42 +0300

    change
    Firefox add

--- src/test/java/SampleTest5.java
+++ src/test/java/SampleTest5.java
@@ -3,7 +3,11 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Story;
 import io.qameta.allure.TmsLink;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.pages.Home;
 import org.pages.Home1;
@@ -13,29 +17,24 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class SampleTest5 {
-    private  WebDriver driver;
+
+    WebDriver driver ;
 
     @Test(description = "Check that Home Page Title is correct.")
-    public void checkHomePageTitleIsCorrect() {
-
-        new Home1(driver).verifyBrowserTitleIsCorrect();
+    public void checkHomePageTitleIsCorrect()
+    {
+        driver.navigate().to("https://www.google.com/");
+        driver.findElement(By.name("q")).sendKeys("SHAFT1");
     }
-
-
-    @Test(description = "Check that Result Stats is not empty after searching for a query.")
-    public void checkResultStatsIsNotEmptyAfterSearchingForAQuery() {
-        new Home1(driver).searchForQuery("");
-    }
-
 
     @BeforeMethod(description = "Setup Browser instance.")
-    public void beforeMethod() {
-        String browser = System.getProperty("targetBrowserName", "edge");
-
+    public void beforeMethod()
+    {
+        //String browser = System.getProperty("targetBrowserName", "edge");
         // Set capabilities based on the browser
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setBrowserName(browser);
-        driver.navigate();
+       // DesiredCapabilities capabilities = new DesiredCapabilities();
+       // capabilities.setBrowserName(browser);
+        driver = new FirefoxDriver();
     }
 
     @AfterMethod(description = "Teardown Browser instance.")
