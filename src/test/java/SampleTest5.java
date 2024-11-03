5436546ae3c92f27957cde75e7000c7b466bc05c
Olaismail316 <olaismail613@gmail.com>
2024-11-03 11:21:12 +0200

    update s5_browser

--- src/test/java/SampleTest5.java
+++ src/test/java/SampleTest5.java
@@ -1,18 +1,9 @@
-import com.shaft.driver.SHAFT;
-import io.qameta.allure.Description;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Story;
-import io.qameta.allure.TmsLink;
+
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.pages.Home;
-import org.pages.Home1;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
